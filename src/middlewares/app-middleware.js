c92a81eba4df7075173ae71a85864fcc12512ef8
edwardsuwirya <edward.suwirya@ffi.co.id>
2019-12-18 17:51:43 +0700

    Add express session 2

--- src/middlewares/app-middleware.js
+++ src/middlewares/app-middleware.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import session from 'express-session';
+import fileUpload from 'express-fileupload';
 
 export default express.Router()
     .use(express.json())
@@ -9,4 +10,12 @@ export default express.Router()
         rolling: true,
         saveUninitialized: false,
         cookie: {maxAge: 15000}
+    })).use(fileUpload({
+        abortOnLimit: true,
+        safeFileNames: true,
+        preserveExtension: true,
+        limits: {
+            fileSize: Number(process.env.FILE_SIZE_LIMIT) * 1024 * 1024,
+            files: Number(process.env.FILE_UPLOAD_COUNT),
+        }
     }))
--- src/routes/category.route.js
+++ src/routes/category.route.js
@@ -3,12 +3,12 @@ import {Router} from 'express';
 
 const CategoryRouter = Router()
     .get('/', function (req, res) {
-        if (req.session.name) {
+        // if (req.session.name) {
             res.set('Content-Type', 'application/json');
             res.send([{categoryId: '1111', categoryName: 'ATK'}, {categoryId: '2222', categoryName: 'Furnitur'}]);
-        } else {
-            res.sendStatus(401);
-        }
+        // } else {
+        //     res.sendStatus(401);
+        // }
     });
 
 export default CategoryRouter;
